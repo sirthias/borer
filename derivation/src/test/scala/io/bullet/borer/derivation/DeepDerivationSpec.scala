74bc9dc6ea6a32d548d6853a541efec7eb36781f
Mathias <mathias@decodified.com>
2020-01-09 14:07:25 +0100

    Split out deep derivation tests, add test for #137, closes #137

---
+++ derivation/src/test/scala/io/bullet/borer/derivation/DeepDerivationSpec.scala
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2019 Mathias Doenitz
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package io.bullet.borer.derivation
+
+import java.nio.charset.StandardCharsets
+
+import io.bullet.borer._
+import utest._
+
+object DeepDerivationSpec extends AbstractBorerSpec {
+
+  def encode[T: Encoder](value: T): String   = Json.encode(value).toUtf8String
+  def decode[T: Decoder](encoded: String): T = Json.decode(encoded getBytes StandardCharsets.UTF_8).to[T].value
+
+  object AnimalAdt {
+    sealed trait Animal
+    case object Wolverine             extends Animal
+    case class Yeti(name: String)     extends Animal
+    sealed trait Dog                  extends Animal
+    case object TheHound              extends Dog
+    case class Labrador(name: String) extends Dog
+    sealed trait Cat                  extends Animal
+    case object TheLastTiger          extends Cat
+    case class Lion(color: String)    extends Cat
+  }
+
+  val tests = Tests {
+
+    "simple" - {
+      import AnimalAdt._
+
+      implicit val codec = ArrayBasedCodecs.deriveAllCodecs[Animal]
+      roundTrip("""["Labrador","Lolle"]""", Labrador("Lolle"): Animal)
+      roundTrip("""["TheLastTiger",[]]""", TheLastTiger: Animal)
+    }
+
+    "custom leaf" - {
+      import AnimalAdt._
+
+      implicit val lionCodec = Codec.bimap[Int, Lion](_ => 0, _ => Lion("roar"))
+      implicit val codec     = ArrayBasedCodecs.deriveAllCodecs[Animal]
+      roundTrip("""["Lion",0]""", Lion("roar"): Animal)
+    }
+
+    "custom inner node" - {
+      import AnimalAdt._
+
+      implicit val catCodec = Codec.bimap[Int, Cat](_ => 0, _ => TheLastTiger)
+      implicit val codec    = ArrayBasedCodecs.deriveAllCodecs[Animal]
+
+      roundTrip("""["Cat",0]""", TheLastTiger: Animal)
+      verifyEncoding(Lion("roar"): Animal, """["Cat",0]""")
+      verifyDecoding("""["Cat",0]""", TheLastTiger: Animal)
+    }
+
+    "recursive" - {
+      sealed trait TreeNode
+      case object Leaf                                 extends TreeNode
+      case class Node(left: TreeNode, right: TreeNode) extends TreeNode
+
+      implicit lazy val codec: Codec[TreeNode] = ArrayBasedCodecs.deriveAllCodecs[TreeNode]
+
+      roundTrip(
+        """["Node",[["Node",[["Leaf",[]],["Node",[["Leaf",[]],["Leaf",[]]]]]],["Leaf",[]]]]""",
+        Node(Node(Leaf, Node(Leaf, Leaf)), Leaf): TreeNode)
+    }
+
+    "generic" - {
+      sealed trait Node[+T]
+      case object Empty                                   extends Node[Nothing]
+      case class Leaf[T](value: T)                        extends Node[T]
+      case class Branch[T](left: Node[T], right: Node[T]) extends Node[T]
+
+      implicit def codec[T: Encoder: Decoder]: Codec[Node[T]] = ArrayBasedCodecs.deriveAllCodecs[Node[T]]
+
+      roundTrip(
+        """["Branch",[["Branch",[["Leaf",1],["Branch",[["Empty",[]],["Leaf",2]]]]],["Leaf",3]]]""",
+        Branch(Branch(Leaf(1), Branch(Empty, Leaf(2))), Leaf(3)): Node[Int])
+    }
+
+    "circular" - {
+      sealed trait Expr
+      case class Add(left: Factor, right: Factor)  extends Expr
+      sealed trait Factor                          extends Expr
+      case class Mult(left: Factor, right: Factor) extends Factor
+      case class Literal(value: Int)               extends Factor
+      case class Parens(expr: Expr)                extends Factor
+
+      implicit lazy val factorCodec: Codec[Factor] = ArrayBasedCodecs.deriveAllCodecs[Factor]
+      implicit lazy val exprCodec: Codec[Expr]     = ArrayBasedCodecs.deriveAllCodecs[Expr]
+
+      roundTrip(
+        """["Add",[["Literal",18],["Parens",["Add",[["Literal",2],["Mult",[["Literal",3],["Literal",4]]]]]]]]""",
+        Add(Literal(18), Parens(Add(Literal(2), Mult(Literal(3), Literal(4))))): Expr)
+    }
+
+    "cross dependencies" - {
+
+      "A" - {
+        sealed trait Expr
+        case class Literal(value: Int) extends Expr
+        case class Neg(lit: Literal)   extends Expr
+
+        implicit lazy val exprCodec: Codec[Expr] = ArrayBasedCodecs.deriveAllCodecs[Expr]
+      }
+
+      "B" - {
+        sealed trait Expr
+        case class Neg(lit: Literal)   extends Expr
+        case class Literal(value: Int) extends Expr
+
+        implicit lazy val exprCodec: Codec[Expr] = ArrayBasedCodecs.deriveAllCodecs[Expr]
+      }
+    }
+
+    "diamond" - {
+      sealed trait A
+      sealed trait B       extends A
+      sealed trait C       extends A
+      case class D(x: Int) extends B with C
+      implicit lazy val codec: Codec[A] = MapBasedCodecs.deriveAllCodecs
+
+      roundTrip("""{"D":{"x":42}}""", D(42): A)
+    }
+  }
+}
